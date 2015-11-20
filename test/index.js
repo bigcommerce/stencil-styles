06f3af6fe3756882b67706466c83ba5f3f476f35
Jared Stoffan <jared.stoffan@bigcommerce.com>
2015-11-19 18:33:34 -0800

    BIG-23364 More refactoring and tests

--- test/index.js
+++ test/index.js
@@ -19,21 +19,97 @@ var _ = require('lodash'),
 describe('Stencil-Styles Plugin', function () {
     beforeEach(function(done) {
         settings = require('./mocks/settings.json');
-        options = { themeSettings: settings };
-        stencilStyles = new StencilStyles();
+        options = {
+            autoprefixerOptions: {},
+            files: {
+                '/mock/path1.scss': 'color: #fff',
+                '/mock/path2.scss': 'color: #000'
+            },
+            themeSettings: settings
+        };
+        stencilStyles = new StencilStyles(options);
 
         done();
     });
 
-    it('should pass a fake test', function(done) {
-        expect(StencilStyles).to.equal(StencilStyles);
+    describe('constructor', function() {
+        it('should set the fullUrls object to empty', function(done) {
+            expect(stencilStyles.fullUrls).to.be.empty();
 
-        done();
+            done();
+        });
+
+        it('should set the options object with defaults', function(done) {
+            expect(stencilStyles.options).to.not.be.empty();
+
+            done();
+        });
     });
 
     describe('compileCss()', function() {
+        var callback;
+
+        beforeEach(function(done) {
+            callback = sinon.spy();
+            sinon.spy(stencilStyles, 'scssCompiler');
+
+            stencilStyles.compileCss('scss', options, callback);
+
+            done();
+        });
+
         it('should call the scss compiler based on the compiler parameter', function(done) {
-            // TODO: Add test logic
+            expect(stencilStyles.scssCompiler.calledOnce).to.equal(true);
+
+            done();
+        });
+
+        it('should call the callback passed in once compilation is complete', function(done) {
+            expect(callback.calledOnce).to.equal(true);
+
+            done();
+        });
+
+        afterEach(function(done) {
+            stencilStyles.scssCompiler.restore();
+
+            done();
+        });
+    });
+
+    describe('scssFunctions', function() {
+        it('should return an array with all required functions', function(done) {
+            var result = stencilStyles.scssFunctions(settings);
+
+            expect(result).to.be.an.object();
+            expect(_.keys(result)).to.contain([
+                'stencilNumber($name, $unit: px)',
+                'stencilColor($name)',
+                'stencilString($name)',
+                'stencilFontFamily($name)',
+                'stencilFontWeight($name)'
+            ]);
+
+            done();
+        });
+    });
+
+    describe('scssImporter()', function() {
+        it('should return an error if files do not exist', function (done) {
+            var result = stencilStyles.scssImporter('/path2', 'other/path1.scss');
+
+            expect(result.constructor).to.equal(Error);
+            expect(result.message).to.equal("other/path2.scss doesn't exist!");
+
+            done();
+        });
+
+        it('should return an object with a file name and content', function (done) {
+            var result = stencilStyles.scssImporter('/path2', '/mock/path1.scss');
+
+            expect(result).to.be.an.object();
+            expect(result).to.include({ file: '/mock/path2.scss' });
+            expect(result).to.include({ contents: options.files['/mock/path2.scss'] });
 
             done();
         });
@@ -47,7 +123,7 @@ describe('Stencil-Styles Plugin', function () {
         });
 
         it('should call the Google font parser for Google fonts', function(done) {
-            stencilStyles.stencilFont(options, 'google-font', 'family');
+            stencilStyles.stencilFont(settings['google-font'], 'family');
 
             expect(stencilStyles.googleFontParser.calledOnce).to.equal(true);
 
@@ -55,7 +131,7 @@ describe('Stencil-Styles Plugin', function () {
         });
 
         it('should return Sass.NULL if the provider is not supported', function(done) {
-            expect(stencilStyles.stencilFont(options, 'bad-font', 'family')).to.equal(Sass.NULL);
+            expect(stencilStyles.stencilFont(settings['bad-font'], 'family')).to.equal(Sass.NULL);
 
             done();
         });
